440de7047bc3e8c18871d0c0172cdc3d14f390d3
joanne <u6511277@anu.edu.au>
2020-10-20 17:36:43 +1100

    Update on login

--- app/src/main/java/com/example/comp2100_6442_s2_2020_group_project/UserRegister.java
+++ app/src/main/java/com/example/comp2100_6442_s2_2020_group_project/UserRegister.java
@@ -17,17 +17,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 
+/**
+ * Reads user input from registeration, and writes it to a xml file so it can be used for log in
+ *
+ *  @author So Young Kwon
+ *  * @uid: 6511277
+ */
+
 public class UserRegister extends AppCompatActivity {
     EditText username;
     EditText userID;
     EditText password;
     EditText passwordConfrim;
     Button signUp;
-/**
- * Reads user input from registeration, and writes it to a xml file so it can be used for log in
- *
- * @author So Young Kwon
- */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
